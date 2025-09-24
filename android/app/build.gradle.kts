97d5bd21fc10d53af352bcd401c8f05c26aacddf
Alldreno <Alldreno.4342401059@students.polibatam.ac.id>
2025-09-24 11:40:24 +0700

    Update app id and security
    Added FLAG_SECURE flag

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.skorify"
+    namespace = "com.github.skorify"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
---
+++ lib/background.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const background = BoxDecoration(
+  image: DecorationImage(
+    image: AssetImage('assets/images/home-background.png'),
+    fit: BoxFit.cover,
+  ),
+);
