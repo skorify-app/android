996d09b9189f7ce37d26e43ce55531d31442d102
Alldreno <Alldreno.4342401059@students.polibatam.ac.id>
2025-09-30 08:37:36 +0700

    Fix main activity error & remove linux

--- android/app/src/main/kotlin/com/example/skorify/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/skorify/MainActivity.kt
@@ -1,5 +1,7 @@
-package com.example.skorify
+package com.github.skorify
 
+import android.os.Bundle
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
