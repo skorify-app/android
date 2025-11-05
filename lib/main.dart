59574fce4e4c95e28baa4f58b4d8a6935292b3d4
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2025-11-05 14:09:16 +0700

    Add flutter_lints package

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Skorify',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Poppins',
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
 
       // Halaman pertama saat aplikasi dibuka
       initialRoute: '/Onboarding_page',
@@ -40,35 +37,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-
-/*
-Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Scaffold(
-        body: Container(
-          decoration: background,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(padding: const EdgeInsets.only(left: 30.0)),
-              Text(
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.white, fontSize: 48),
-                'SKORIFY',
-              ),
-              Text(
-                textAlign: TextAlign.left,
-                style: TextStyle(color: Colors.white, fontSize: 20),
-                'Aplikasi CBT Test Potensi Akademik',
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }*/
