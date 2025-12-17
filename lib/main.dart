dc111db3d586f8b96a5692d482add1239413c8d3
Alldreno Hosea <Alldreno.4342401059@students.polibatam.ac.id>
2025-12-18 01:10:29 +0700

    Remove mockup result screens and change app display name

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:skorify/pages/activity_screen.dart';
-import 'package:skorify/pages/detail_result_ump/detail_result_ump_inggris.dart';
-import 'package:skorify/pages/detail_result_ump/detail_result_ump_ct.dart';
-import 'package:skorify/pages/detail_result_ump/detail_result_ump_indonesia.dart';
-import 'package:skorify/pages/detail_result_ump/detail_result_ump_mtk.dart';
-import 'package:skorify/pages/detail_result_ump/detail_result_ump_sains.dart';
 import 'package:skorify/pages/homepage.dart';
 import 'package:skorify/pages/loading_detail_screen.dart';
 import 'package:skorify/pages/start_screen.dart';
-import 'package:skorify/pages/test_result/test_result_ct.dart';
-import 'package:skorify/pages/test_result/test_result_indonesia.dart';
-import 'package:skorify/pages/test_result/test_result_inggris.dart';
-import 'package:skorify/pages/test_result/test_result_mtk.dart';
-import 'package:skorify/pages/test_result/test_result_sains.dart';
-import 'package:skorify/pages/test_result/test_result_ump.dart';
 import 'pages/onboarding_page.dart';
 import 'pages/login_pages.dart';
 import 'pages/register_pages.dart';
@@ -87,32 +76,6 @@ Widget _getPage(String? route) {
       return const ActivityScreen();
     case '/account':
       return const AccountScreen();
-
-    // Test Result
-    case '/test_result_ump':
-      return const TestResultUmp();
-    case '/test_result_sains':
-      return const TestResultSains();
-    case '/test_result_mtk':
-      return const TestResultMtk();
-    case '/test_result_ct':
-      return const TestResultCt();
-    case '/test_result_inggris':
-      return const TestResultInggris();
-    case '/test_result_indonesia':
-      return const TestResultIndonesia();
-
-    // Detail Result
-    case '/detail_result_ump_mtk':
-      return const DetailResultUmpMtk();
-    case '/detail_result_ump_sains':
-      return const DetailResultUmpSains();
-    case '/detail_result_ump_ct':
-      return const DetailResultUmpCt();
-    case '/detail_result_ump_inggris':
-      return const DetailResultUmpInggris();
-    case '/detail_result_ump_indonesia':
-      return const DetailResultUmpIndonesia();
 
     default:
       return const StartScreen();
